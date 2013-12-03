f35ff25a631184ba8835b167fcbeed594dd37362
Eric Bollens <ebollens@ucla.edu>
2013-12-03 11:09:39 -0800

    Update callforward to use WebBlocks-kit-core

--- src/ucla/webblocks/theme/Rakefile-config.rb
+++ src/ucla/webblocks/theme/Rakefile-config.rb
@@ -1,16 +1,35 @@
 require 'pathname'
+
+# This is the location where the theme and WebBlocks-kit-core directories reside
 rootdir = Pathname.new(__FILE__).realpath.parent.parent
 
-WebBlocks.config[:build][:debug] = false
+# True to compile in debug mode (no minification)
+WebBlocks.config[:build][:debug] = true
 
 # The directory into which WebBlocks is built
 WebBlocks.config[:build][:dir] = "#{rootdir}/theme/static/blocks"
 
 # The directory where sources for the build are located
 WebBlocks.config[:src][:dir] = "#{rootdir}/theme/blocks"
-WebBlocks.config[:src][:core][:dir] = "#{rootdir}/WebBlocks/src/core"
-WebBlocks.config[:src][:adapters][:dir] = "#{rootdir}/WebBlocks/src/adapter"
-WebBlocks.config[:src][:extension][:dir] = "#{rootdir}/WebBlocks/src/extension"
 
+# Location of WebBlocks core components (config.rb, definitions, core adapter)
+WebBlocks.config[:src][:core][:dir] = "#{rootdir}/WebBlocks-kit-core/_blocks/src/core"
+
+# Location of WebBlocks adapters
+WebBlocks.config[:src][:adapters][:dir] = "#{rootdir}/WebBlocks-kit-core/_blocks/src/adapter"
+
+# Location of WebBlocks adapters
+WebBlocks.config[:src][:extension][:dir] = "#{rootdir}/WebBlocks-kit-core/extension"
+
+WebBlocks.config[:src][:extensions] << "blocks"
+WebBlocks.config[:src][:extensions] << "ucla"
+WebBlocks.config[:src][:extensions] << "WebBlocks.Blocks.js"
+
+# Do not use an adapter
 WebBlocks.config[:src][:adapter] = false
-WebBlocks.config[:src][:extensions] << 'ucla'
+
+# Uncomment this line if site is already built with Bootstrap:
+#WebBlocks.config[:src][:adapter] = 'bootstrap'
+
+# Use the ARIA Mapper package
+WebBlocks.config[:build][:packages] << :jqueryariamapper
